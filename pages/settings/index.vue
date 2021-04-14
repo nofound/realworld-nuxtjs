0702fe0a0864d166a7db757ffd6cf0943207f14f
王晓东 <1099079410@qq.com>
2021-04-14 21:27:43 +0800

    ''

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -6,22 +6,22 @@
         <div class="col-md-6 offset-md-3 col-xs-12">
           <h1 class="text-xs-center">Your Settings</h1>
 
-          <form>
+          <form @submit.prevent="handleSubmit">
             <fieldset>
                 <fieldset class="form-group">
-                  <input class="form-control" type="text" placeholder="URL of profile picture">
+                  <input v-model="user.image"  class="form-control" type="text" placeholder="URL of profile picture">
                 </fieldset>
                 <fieldset class="form-group">
-                  <input class="form-control form-control-lg" type="text" placeholder="Your Name">
+                  <input v-model="user.username" class="form-control form-control-lg" type="text" placeholder="Your Name">
                 </fieldset>
                 <fieldset class="form-group">
-                  <textarea class="form-control form-control-lg" rows="8" placeholder="Short bio about you"></textarea>
+                  <textarea v-model="user.bio" class="form-control form-control-lg" rows="8" placeholder="Short bio about you"></textarea>
                 </fieldset>
                 <fieldset class="form-group">
-                  <input class="form-control form-control-lg" type="text" placeholder="Email">
+                  <input v-model="user.email" class="form-control form-control-lg" type="text" placeholder="Email">
                 </fieldset>
                 <fieldset class="form-group">
-                  <input class="form-control form-control-lg" type="password" placeholder="Password">
+                  <input v-model="user.password" class="form-control form-control-lg" type="password" placeholder="Password">
                 </fieldset>
                 <button class="btn btn-lg btn-primary pull-xs-right">
                   Update Settings
@@ -36,9 +36,38 @@
 </template>
 
 <script>
+import { getUserInfo, updateUserInfo } from '@/api/user'
 export default {
   middleware: 'authenticated',
-  name: 'SettingsIndex'
+  name: 'SettingsIndex',
+  data () {
+    return {
+      user: {
+          bio: "",
+          email: "",
+          image: "",
+          username: "",
+          password: ""
+      }
+    }
+  },
+  created () {
+      this.getDetail()
+  },
+  methods: {
+      async getDetail () {
+          const { data } = await getUserInfo()
+          this.user =  { ...data.user }
+      },
+      handleSubmit () {
+        const param = { user: this.user }
+        updateUserInfo(param).then(res => {
+          if (res.code === '200'){
+            
+          }
+        })
+      }
+  }
 }
 </script>
 
